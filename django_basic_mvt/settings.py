c692ae2e33bd0de80d568c6d0a4131d0f4e90319
jakob-bren <jakob.brennan@gmail.com>
2024-02-14 11:19:00 +0000

    add secret key & debug false

--- django_basic_mvt/settings.py
+++ django_basic_mvt/settings.py
@@ -26,10 +26,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$=-#wpd+px16inuk92j7*!t@=)%cw1nmb5@1o((e0hf_iyrg@5'
+# SECRET_KEY = 'django-insecure-$=-#wpd+px16inuk92j7*!t@=)%cw1nmb5@1o((e0hf_iyrg@5'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', '192.168.1.111', 'evie.mrman', 'evie.cat']
 
